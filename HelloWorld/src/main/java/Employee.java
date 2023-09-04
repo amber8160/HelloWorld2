ee8c467db90361b910ebc43f9ac89938168155be
alber <alber@192.168.0.86>
2023-09-04 23:41:50 +0800

    third commit

--- HelloWorld/src/main/java/Employee.java
+++ HelloWorld/src/main/java/Employee.java
@@ -3,4 +3,29 @@ public class Employee {
 	private String name;
 	private String phone;
 	private String address;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 }
